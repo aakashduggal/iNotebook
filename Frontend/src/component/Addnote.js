04097ec7932675b6e760f353f466c5da650309f2
Aakash Duggal <aakashduggal2407@gmail.com>
2026-03-02 22:29:03 +0530

    added first CRUD operation Create Note or addNote in app

---
+++ Frontend/src/component/Addnote.js
@@ -0,0 +1,38 @@
+import React, {useContext,useState} from "react";
+import NoteContext from '../context/NoteContext'
+
+const Addnote = () => {
+const {addNote} = useContext(NoteContext)
+
+const [note, setNote] = useState({title: "", description: "", tag: "default"})
+
+const handleSubmit = (e)=>{
+    e.preventDefault()
+   addNote(note.title, note.description, note.tag)
+}
+
+const onChange = (e)=>{
+
+    setNote({...note, [e.target.name] : e.target.value})
+
+}
+
+    return (
+    <div className='container my-3'>
+        <form>
+            <div className="mb-3">
+                <label htmlFor="title" className="form-label">Title</label>
+                <input type="text" className="form-control" id="title" name="title" onChange={onChange} aria-describedby="emailHelp" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="description" className="form-label">Description</label>
+                <input type="text" className="form-control" id="description" name="description" onChange={onChange}/>
+            </div>
+            
+            <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+        </form>
+    </div>
+    )   
+}
+
+export default Addnote
