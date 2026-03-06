3bc309f6da21fb9eb7b774df8c98d68931b42aa6
Aakash Duggal <aakashduggal2407@gmail.com>
2026-03-06 10:05:36 +0530

    added addNote API call

--- Frontend/src/context/NoteState.js
+++ Frontend/src/context/NoteState.js
@@ -4,44 +4,7 @@ import NoteContext from "./NoteContext";
 const NoteState = (props)=>{
   const host = "http://localhost:5000"
 
-    const initialNotes = [
-  {
-    "_id": "69a03296e0b6af3510e3e534",
-    "user": "699ef5ad1a5cd52b1c4b8d33",
-    "title": "My title1 ",
-    "description": "Please wake up early",
-    "tag": "personal",
-    "date": "2026-02-26T11:46:30.819Z",
-    "__v": 0
-  },
-  {
-    "_id": "69a03296e0b6af3510e3e536",
-    "user": "699ef5ad1a5cd52b1c4b8d33",
-    "title": "My title2 ",
-    "description": "Please wake up early",
-    "tag": "personal",
-    "date": "2026-02-26T11:46:30.930Z",
-    "__v": 0
-  },
-  {
-    "_id": "69a03297e0b6af3510e3e538",
-    "user": "699ef5ad1a5cd52b1c4b8d33",
-    "title": "My title3 ",
-    "description": "Please wake up early",
-    "tag": "personal",
-    "date": "2026-02-26T11:46:31.206Z",
-    "__v": 0
-  },
-  {
-    "_id": "69a43f10149ad73f8cb70k1af",
-    "user": "699ef5ad1a5cd52b1c4b8d33",
-    "title": "My title4 ",
-    "description": "Please wake up early",
-    "tag": "personal",
-    "date": "2026-03-01T13:28:48.470Z",
-    "__v": 0
-  }
-]
+    const initialNotes = []
 
 const fetchNotes = async ()=>{
   const response = await fetch(`${host}/api/notes/getAllnotes`,{
@@ -57,7 +20,15 @@ const fetchNotes = async ()=>{
 const [notes, setNotes] = useState(initialNotes)
 
 // Add a note
-const addNote = (title, description, tag)=>{
+const addNote = async (title, description, tag)=>{
+  const response = await fetch(`${host}/api/notes/addNote`,{
+    method: 'POST',
+    headers:{
+      'content-type' : 'application/json',
+      'auth-token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjk5ZWY1YWQxYTVjZDUyYjFjNGI4ZDMzIn0sImlhdCI6MTc3MjA5ODU3Mn0.D4gaJtCLckLP7T3thRZ7XrMPfeFnafPJX0tU012CTfc'
+    },
+    body: json.stringify({title, description, tag})
+  })
   const note = {
     "_id": "69a43f10149ad73f8cb701af",
     "user": "699ef5ad1a5cd52b1c4b8d33",
